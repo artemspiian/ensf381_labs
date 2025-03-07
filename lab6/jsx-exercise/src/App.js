26768262e5038ecef42b890371b774b8966c79a5
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2025-03-07 11:22:14 -0700

    Exercise 1 part 3

--- lab6/jsx-exercise/src/App.js
+++ lab6/jsx-exercise/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+let currentYear = (new Date()).getFullYear()
+
+
 function App() {
   return (
     <div>
       <h1>ENSF - 381: Full Stack Web Development</h1>
       <p>React Componenets</p>
+      <p>{currentYear}</p>
     </div>
   );
 }
