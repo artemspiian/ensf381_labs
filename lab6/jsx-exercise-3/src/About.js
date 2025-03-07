f315a27f3ee5a5d09c54c54c17c02904ea6cb988
Artem Spiian <artem@spiian.cc>
2025-03-07 13:03:24 -0700

    Add Lab6 files to main from artem ensf381

---
+++ lab6/jsx-exercise-3/src/About.js
@@ -0,0 +1,11 @@
+function About({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h2>About Us</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ lab6/jsx-exercise-3/src/Home.js
@@ -0,0 +1,11 @@
+function Home({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h2>Welcome to the Home Page</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Home;
