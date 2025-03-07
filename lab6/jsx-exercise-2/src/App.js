8428f57189d458b353e74ce73a9567f5d900a781
Artem Spiian <artem@spiian.cc>
2025-03-07 12:49:07 -0700

    Finish Lab 6 ensf381

---
+++ lab6/jsx-exercise-2/src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import About from "./About.js";
+import Contact from "./Contact.js";
+
+function App() {
+  const header = <h1>ENSF-381: Full Stack Web Development</h1>;
+  const reactComponents = <p>React Components</p>;
+  const currentYear = <p> {new Date().getFullYear()} </p>;
+  const isLoggedIn = true;
+  const isLoggedInString = (
+    <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
+  );
+
+  return (
+    <div>
+      {header}
+      {reactComponents}
+      {currentYear}
+      {isLoggedInString}
+      <Home title="Home Page" description="Welcome to our website."/>
+      <About title="About Us" description="We are passionate about delivering quality experiences."/>
+      <Contact title="Contact Us" description="Feel free to reach out to us via email or phone."/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab6/jsx-exercise-3/src/Contact.js
@@ -0,0 +1,11 @@
+function Contact({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h2>Contact Us</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Contact;
